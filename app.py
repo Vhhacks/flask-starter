aaa139cefa10f5b5f112fe80de80459bfdf93904
Frank Hui <huifrank@live.com>
2021-03-24 00:03:24 -0700

    Add template, assets, and style

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+
 app = Flask(__name__)
+
+@app.route('/assets/<path>')
+def send_assets(path):
+    return send_from_directory('assets', path)
+
+@app.route('/css/<path>')
+def send_style(path):
+    return send_from_directory('css', path)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
